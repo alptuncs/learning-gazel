871c6304fe2ccb72dc6d3fffb02c662f60e7180f
alptunc.sever <alptuncsever@gmail.com>
2022-08-04 11:36:32 +0300

    Added Unit Tests

--- Gazel-Tutorial/test/Gazel.Tutorial.Test.ProductManagement/ProductTests.cs
+++ Gazel-Tutorial/test/Gazel.Tutorial.Test.ProductManagement/ProductTests.cs
@@ -13,6 +13,85 @@ namespace Inventiv.Todo.Test.Products
             Config.RootNamespace = "Inventiv";
         }
 
-        
+        public void CreateProduct__creates_a_product_using_given_name()
+        {
+            var productManager = Context.Get<ProductManager>();
+
+            BeginTest();
+
+            var actual = productManager.CreateProduct("Cips", 10.25F, 125);
+
+            Verify.ObjectIsPersisted(actual);
+            Assert.AreEqual("Cips", actual.Name);
+        }
+
+        [Test]
+        public void CreateProduct__creates_a_product_using_given_price()
+        {
+            var productManager = Context.Get<ProductManager>();
+
+            BeginTest();
+
+            var actual = productManager.CreateProduct("Cips", 10.25F, 125);
+
+            Verify.ObjectIsPersisted(actual);
+            Assert.AreEqual(10.25F, actual.Price);
+        }
+
+        [Test]
+        public void CreateProduct__creates_a_product_using_given_stock()
+        {
+            var productManager = Context.Get<ProductManager>();
+
+            BeginTest();
+
+            var actual = productManager.CreateProduct("Cips", 10.25F, 125);
+
+            Verify.ObjectIsPersisted(actual);
+            Assert.AreEqual(125, actual.Stock);
+        }
+
+        [Test]
+        public void Products_ByPrice__filters_products_by_price()
+        {
+            var productManager = Context.Get<ProductManager>();
+
+            productManager.CreateProduct("Cips", 14.99F, 10);
+
+            BeginTest();
+
+            var actual = Context.Query<Products>.ByPrice(14.99F);
+
+            Assert.AreEqual(14.99F, actual[0].Price);
+        }
+
+        [Test]
+        public void Products_ByStock__filters_products_by_stock()
+        {
+            var productManager = Context.Get<ProductManager>();
+
+            productManager.CreateProduct("Cips", 14.99F, 10);
+
+            BeginTest();
+
+            var actual = Context.Query<Products>.ByStock(true);
+
+            Assert.AreEqual(true, actual[0].StockInfo);
+
+        }
+        [Test]
+        public void Products_ByStock__filters_products_by_id()
+        {
+            var productManager = Context.Get<ProductManager>();
+
+            productManager.CreateProduct("Cips", 14.99F, 10);
+
+            BeginTest();
+
+            var actual = Context.Query<Products>.ByStock(true);
+
+            Assert.AreEqual(true, actual[0].StockInfo);
+
+        }
     }
 }
